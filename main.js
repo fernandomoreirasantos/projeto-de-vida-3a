5284d0733b68a029d81aa6331ab14bdf67c11f7c
fernandomoreirasantos <133687014+fernandomoreirasantos@users.noreply.github.com>
2025-06-03 13:46:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0;i <botoes.length;i++);
     botoes[i].onclick = function(){
-      botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
       
 for (let j=0;i <botoes.length;j++);{
     botoes[j].classList.remove("ativo");
+    textos[i].classList.add("ativo");
 
     }
     console.log();
+    const contadores= document.querySelectorAll("contador");
+     const tempoObjetivo1 = new date ("")
+     const tempoObjetivo2= new date ("")
+     const tempoObjetivo3= new date ("")
+     const tempoObjetivo4= new date ("")
 }
